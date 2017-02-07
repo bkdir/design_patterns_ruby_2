898daad1c092fe6dd403291cf0d770a76bbe866b
bkdir <direkburak1@gmail.com>
2017-02-07 00:21:19 -0500

    Wrap template method pattern lib and spec file with module TemplatePattern. Necessary for testing

--- lib/template_method.rb
+++ lib/template_method.rb
@@ -1,64 +1,66 @@
-class Hero
-  attr_reader :damage, :abilities
+module TemplateMethod
+  class Hero
+    attr_reader :damage, :abilities
 
-  def initialize
-   initialize_stats
+    def initialize
+     initialize_stats
 
-   @damage = damage_rating
-   @abilities = occupation_abilities
+     @damage = damage_rating
+     @abilities = occupation_abilities
+    end
+
+    def initialize_stats
+    end
+
+    def damage_rating
+      10
+    end
+
+    def occupation_abilities
+      []
+    end
+
+    def attack
+      "Attack dealing #@damage damage"
+    end
+    
+    def greet
+      greeting = ["Hello"]
+      greeting << unique_greeting
+      greeting
+    end
+
+    def unique_greeting
+      raise NoMethodError, "must be implemented in subclasses"
+    end
+
   end
 
-  def initialize_stats
+  class Warrior < Hero
+    def damage_rating
+      15
+    end
+
+    def occupation_abilities
+      [:strike]
+    end
+
+    def unique_greeting
+      "Warrior is ready to fight"
+    end
   end
 
-  def damage_rating
-    10
-  end
+  class Magician < Hero
+    def damage_rating
+      7
+    end
 
-  def occupation_abilities
-    []
-  end
+    def occupation_abilities
+      [:magic_arrow]
+    end
 
-  def attack
-    "Attack dealing #@damage damage"
-  end
-  
-  def greet
-    greeting = ["Hello"]
-    greeting << unique_greeting
-    greeting
-  end
-
-  def unique_greeting
-    raise NoMethodError, "must be implemented in subclasses"
-  end
-
-end
-
-class Warrior < Hero
-  def damage_rating
-    15
-  end
-
-  def occupation_abilities
-    [:strike]
-  end
-
-  def unique_greeting
-    "Warrior is ready to fight"
-  end
-end
-
-class Magician < Hero
-  def damage_rating
-    7
-  end
-
-  def occupation_abilities
-    [:magic_arrow]
-  end
-
-  def unique_greeting
-    "Magician is ready to fight"
+    def unique_greeting
+      "Magician is ready to fight"
+    end
   end
 end
--- spec/template_method_spec.rb
+++ spec/template_method_spec.rb
@@ -1,59 +1,61 @@
 require "template_method"
 
-describe "Template Method Pattern" do
-  context "Default Hero" do
-    let(:hero) { Hero.new }
+module TemplateMethod
+  describe "Template Method Pattern" do
+    context "Default Hero" do
+      let(:hero) { Hero.new }
 
-    it "has default damage rating of 10" do
-      expect(hero.damage).to eq(10)
+      it "has default damage rating of 10" do
+        expect(hero.damage).to eq(10)
+      end
+
+      it "can attack" do
+        expect(hero.attack).to eq("Attack dealing 10 damage")
+      end
+
+      it "requires subclasses to implement unique_greeting" do
+        expect{ hero.greet }.to raise_exception(NoMethodError)
+      end
     end
 
-    it "can attack" do
-      expect(hero.attack).to eq("Attack dealing 10 damage")
+    context "Warrior" do
+      let(:hero) { Warrior.new }
+
+      it "has default damage rating of 15" do
+        expect(hero.damage).to eq(15)
+      end
+
+      it "can attack" do
+        expect(hero.attack).to eq("Attack dealing 15 damage")
+      end
+
+      it "has Strike special ability" do
+        expect(hero.abilities).to include(:strike)
+      end
+
+      it "greets other characters" do
+        expect( hero.greet ).to eq(["Hello", "Warrior is ready to fight"])
+      end
     end
 
-    it "requires subclasses to implement unique_greeting" do
-      expect{ hero.greet }.to raise_exception(NoMethodError)
-    end
-  end
+    context "Magician" do
+      let(:hero) { Magician.new }
 
-  context "Warrior" do
-    let(:hero) { Warrior.new }
+      it "has default damage rating of 7" do
+        expect(hero.damage).to eq(7)
+      end
 
-    it "has default damage rating of 15" do
-      expect(hero.damage).to eq(15)
-    end
+      it "can attack" do
+        expect(hero.attack).to eq("Attack dealing 7 damage")
+      end
 
-    it "can attack" do
-      expect(hero.attack).to eq("Attack dealing 15 damage")
-    end
+      it "has Magic Arrow special ability" do
+        expect(hero.abilities).to include(:magic_arrow)
+      end
 
-    it "has Strike special ability" do
-      expect(hero.abilities).to include(:strike)
-    end
-
-    it "greets other characters" do
-      expect( hero.greet ).to eq(["Hello", "Warrior is ready to fight"])
-    end
-  end
-
-  context "Magician" do
-    let(:hero) { Magician.new }
-
-    it "has default damage rating of 7" do
-      expect(hero.damage).to eq(7)
-    end
-
-    it "can attack" do
-      expect(hero.attack).to eq("Attack dealing 7 damage")
-    end
-
-    it "has Magic Arrow special ability" do
-      expect(hero.abilities).to include(:magic_arrow)
-    end
-
-    it "greets other characters" do
-      expect( hero.greet ).to eq(["Hello", "Magician is ready to fight"])
+      it "greets other characters" do
+        expect( hero.greet ).to eq(["Hello", "Magician is ready to fight"])
+      end
     end
   end
 end
